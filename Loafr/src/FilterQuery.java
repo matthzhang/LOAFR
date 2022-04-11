8c3810e843993217a7b3773f77fe93e46aacbefd
Rami <dyab0001@umn.edu>
2022-04-11 17:32:21 -0500

    adding missing syntax

--- Loafr/src/FilterQuery.java
+++ Loafr/src/FilterQuery.java
@@ -1,37 +1,36 @@
-public class FilterQuery extends DataQuery{
+public class FilterQuery extends DataQuery {
 
     // FIELDS ----------------------------------------------------------------------------------------------------------
     private String[] filterCriteria;
 
     // CONSTRUCTORS ----------------------------------------------------------------------------------------------------
-    public FilterQuery (){
+    public FilterQuery() {
     }
 
     // SETTERS & GETTERS -----------------------------------------------------------------------------------------------
-    public String[] getFilterCriteria(){
+    public String[] getFilterCriteria() {
         return filterCriteria;
     }
 
-    public void setSearchTarget(String f){
+    public void setSearchTarget(String f) {
         filterCriteria[0] = f;
     }
 
 
-
-
     // METHODS ---------------------------------------------------------------------------------------------------------
-    public String[] filterByComponent(Database data){
+    public String[] filterByComponent(Database data) {
         String[] results = {""};
         return results;
     }
 
-    public String[] filterByField(Database data){
+    public String[] filterByField(Database data) {
         String[] results = {""};
         return results;
     }
 
-    public String[] filterByTime(Database data){
+    public String[] filterByTime(Database data) {
         String[] results = {""};
         return results;
     }
+}
 
