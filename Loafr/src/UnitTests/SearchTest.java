1edfadfef0418ec4608af0c0806d7f2f618510c8
CODEX\adise <adisesh.viruru@gmail.com>
2022-04-11 17:53:20 -0500

    final searchtests

--- Loafr/src/UnitTests/SearchTest.java
+++ Loafr/src/UnitTests/SearchTest.java
@@ -9,7 +9,7 @@ public class SearchTest {
         Database data=null;
         boolean getFile = true;
         try {
-            data = new Database("tests\\sampleTestForUnitT1.csv"); // initialize database with file content   
+            data = new Database("\\tests\\sampleTestForUnitT1.csv"); // initialize database with file content   
             search=new SearchQuery("HeartMonitor");
             getFile = false;
         } catch (FileNotFoundException e) {
@@ -23,23 +23,30 @@ public class SearchTest {
         System.out.println("SEARCH TEST 1: FAILED");
         
         }
-
-        search.setSearchTarget("heat-degree");
-        System.out.println(search.getSearchTarget());
-        if(search.searchByField(data)==10){
+        search.setSearchTarget("time-stamp");
+        if(search.searchByField(data)==15){
             System.out.println("SEARCH TEST 2: SUCCESS");
         }
         else{
             
         System.out.println("SEARCH TEST 2: FAILED");
         }
-        search.setSearchTarget("2.00");
-        if(search.searchByTime(data)==3){
+
+        search.setSearchTarget("heat-degree");
+        if(search.searchByField(data)==10){
             System.out.println("SEARCH TEST 3: SUCCESS");
         }
         else{
             
         System.out.println("SEARCH TEST 3: FAILED");
+        }
+        search.setSearchTarget("2.00");
+        if(search.searchByTime(data)==3){
+            System.out.println("SEARCH TEST 4: SUCCESS");
+        }
+        else{
+            
+        System.out.println("SEARCH TEST 4: FAILED");
         }
         
     }
