972a138d77defbcc302fb4a0a52186c7cca507ba
alexmking <92904100+alexmking@users.noreply.github.com>
2022-04-11 16:48:24 -0500

    Update FilterQuery.java

--- Loafr/src/FilterQuery.java
+++ Loafr/src/FilterQuery.java
@@ -1,2 +1,37 @@
-public class FilterQuery extends DataQuery {
-}
+public class FilterQuery extends DataQuery{
+
+    // FIELDS ----------------------------------------------------------------------------------------------------------
+    private String[] filterCriteria;
+
+    // CONSTRUCTORS ----------------------------------------------------------------------------------------------------
+    public FilterQuery (){
+    }
+
+    // SETTERS & GETTERS -----------------------------------------------------------------------------------------------
+    public String[] getFilterCriteria(){
+        return filterCriteria;
+    }
+
+    public void setSearchTarget(String f){
+        filterCriteria[0] = f;
+    }
+
+
+
+
+    // METHODS ---------------------------------------------------------------------------------------------------------
+    public String[] filterByComponent(Database data){
+        String[] results = {""};
+        return results;
+    }
+
+    public String[] filterByField(Database data){
+        String[] results = {""};
+        return results;
+    }
+
+    public String[] filterByTime(Database data){
+        String[] results = {""};
+        return results;
+    }
+
