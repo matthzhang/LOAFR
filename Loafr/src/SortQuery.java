e0330387c2699e5038e73537d47816550db22b40
alexmking <92904100+alexmking@users.noreply.github.com>
2022-04-11 21:35:35 -0500

    Update SortQuery.java

--- Loafr/src/SortQuery.java
+++ Loafr/src/SortQuery.java
@@ -1,8 +1,9 @@
 public class SortQuery extends DataQuery{
 
+    
     // FIELDS ----------------------------------------------------------------------------------------------------------
     private String sortTarget;
-
+    private String[] results = new String[20];
 
     // CONSTRUCTORS ----------------------------------------------------------------------------------------------------
     public SortQuery (String target){
@@ -20,11 +21,11 @@ public class SortQuery extends DataQuery{
 
     // METHODS ---------------------------------------------------------------------------------------------------------
     public void sortAscending(String field){
-
+        results[0] = 5;
     }
 
     public void sortDescending(String field){
-
+        results[0] = 10; 
     }
 
 
